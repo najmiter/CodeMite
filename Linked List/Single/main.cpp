f9717f121181caaedadb19f85deb7e0f495f6867
Najam <85332859+najmiter@users.noreply.github.com>
2023-07-19 22:35:15 +0500

    Update main.cpp

--- Linked List/Single/main.cpp
+++ Linked List/Single/main.cpp
@@ -1,4 +1,25 @@
 #include "list.hpp"
 
-int main() {
+using Int32List = List<int32_t>;
+
+int main() 
+{
+  Int32List list;
+
+  list.add(90);
+  list.add(9);
+  list.add(912);
+
+  list.insert(12523, 3);
+
+  try 
+  {
+    list.insert(12, 0);
+  }
+  catch (const std::out_of_range& error) 
+  {
+    std::cerr << error.what() << std::endl;
+  }
+  
+  Int32List::display(list);
 }
