23cb003275651f01f4aeffe96643befa1d70dd56
Najam <85332859+najmiter@users.noreply.github.com>
2023-07-31 12:23:42 +0500

    Update main.cpp

--- Linked List/Single/main.cpp
+++ Linked List/Single/main.cpp
@@ -7,19 +7,18 @@ int main()
   Int32List list;
 
   list.add(90);
-  list.add(9);
-  list.add(912);
+  list.add(120);
+  list.add(19);
+  list.add(4);
+  list.add(54);
+  list.add(21);
 
-  list.insert(12523, 3);
 
-  try 
-  {
-    list.insert(12, 0);
-  }
-  catch (const std::out_of_range& error) 
-  {
-    std::cerr << error.what() << std::endl;
-  }
-  
   Int32List::display(list);
+
+  list.reverse();
+
+  Int32List::display(list);
+
+  
 }
