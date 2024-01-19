bf99a51eb118469ca99422b3ca07b556d5f116ae
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-19 12:44:41 +0500

    Create 51_exceptions.cs

---
+++ C# Course/51_exceptions.cs
@@ -0,0 +1,46 @@
+namespace CodeMite
+{
+     class CustomException : Exception
+     {
+          public CustomException(string msg) : base(msg)
+          {
+
+          }
+     }
+     class MainProgram
+     {
+          static void Main(string[] _)
+          {
+               try
+               {
+                    bool failure = false;
+                    bool otherCondition = false;
+                    if (failure)
+                    {
+                         throw new CustomException("Some kind of exception occurred.");
+                    }
+                    else if (otherCondition)
+                    {
+                         throw new ArgumentException("Invalid arguments.");
+                    }
+
+                    Console.WriteLine("Processing the data...");
+
+                    // process the data
+                    ///
+               }
+               catch (ArgumentException ex)
+               {
+                    Console.WriteLine(ex.Message);
+               }
+               catch (CustomException ex)
+               {
+                    Console.WriteLine(ex.Message);
+               }
+               finally
+               {
+                    Console.WriteLine("Finally freeing the resources...");
+               }
+          }
+     }
+}
