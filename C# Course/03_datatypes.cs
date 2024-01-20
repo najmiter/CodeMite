7e997927548ff1fce2cf965085e00bf52126b0d7
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-20 13:14:45 +0500

    Create 03_datatypes.cs

---
+++ C# Course/03_datatypes.cs
@@ -0,0 +1,22 @@
+class MainProgram
+{
+    public static void Main(string[] _)
+    {
+        // byte, sbyte, short, ushort, int, uint, long, ulong, char
+        int number = 12;
+        long l = 12L;
+        uint = 12U;
+
+        // int.MaxValue
+
+        bool b = false;
+        char c = 'C';
+        string s = "CodeMite has created this course for free for everyone.";
+        String str = "You can use Pascal casing for types in C# too to stay consistent among types.";
+
+        float f = 12.0f;    // 4 bytes
+        double d = 12.0d;    // 8 bytes
+        decimal de = 12.0m;    // 16 bytes
+        
+    }
+}
