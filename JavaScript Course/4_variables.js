18d45569c91f1527b11c63c4a4edc94a92a75a6e
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-25 22:55:01 +0500

    Create 4_variables.js

---
+++ JavaScript Course/4_variables.js
@@ -0,0 +1,13 @@
+const number = 12;
+let string = "this is a string";
+let bool = true;
+let array = [1, 2, "string", 4];
+let object = { a: "value" };
+let undef = undefined; // undefined
+let absence = null;
+let nan = NaN;
+
+// number = "string"; // ERROR
+// number = [1, 2, 3]; // ERROR
+
+console.log(number);
