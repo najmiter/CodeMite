9aca8605098a290a281f8ee5ea2bad33c7e4a3a5
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-26 23:20:26 +0500

    Create 8_logical_ops_short_circuiting.js

---
+++ JavaScript Course/8_logical_ops_short_circuiting.js
@@ -0,0 +1,19 @@
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// console.log(!true);
+// console.log(!false);
+
+// false && true;
+// true || false;
+
+const condition = true;
+
+true && console.log("TRUE");
