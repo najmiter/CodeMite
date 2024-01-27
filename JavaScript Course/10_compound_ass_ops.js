dbce528b20332f9615b867a61ec48f1ac3a644db
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-27 14:28:20 +0500

    Create 10_compound_ass_ops.js

---
+++ JavaScript Course/10_compound_ass_ops.js
@@ -0,0 +1,19 @@
+let number = 10;
+
+// number = number + 5; // 10 + 5
+// number = number * 10; // 15 * 10
+
+// number += 5 * 10;
+// number = number + (5 * 10); //-> 50 + 10
+// console.log(number);
+
+// number *= 5 + 5;
+// number = number * (5 + 5);
+// console.log(number);
+
+// number = 3 ** 30;
+// console.log(number);
+
+number = 49;
+number **= 1 / 2;
+console.log(number);
