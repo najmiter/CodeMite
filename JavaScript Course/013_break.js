870980dced256106712609381985db9e18d0891e
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-28 14:53:27 +0500

    Create 013_break.js

---
+++ JavaScript Course/013_break.js
@@ -0,0 +1,18 @@
+let counter = 1;
+
+// while (true) {
+//     console.log("coutner:", counter);
+//     counter++; // 7
+
+//     if (counter === 7) {
+//         break;
+//     }
+// }
+
+for (let i = 1; i; i++) { // 7
+    console.log("i:", i); // 7
+
+    if (i === 7) { // 7 === 7
+        break;
+    }
+}
