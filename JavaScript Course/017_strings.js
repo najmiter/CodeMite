d7aceddd79c07e5b20f73cf64607aaddd2e36bb2
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-30 12:47:25 +0500

    Create 017_strings.js

---
+++ JavaScript Course/017_strings.js
@@ -0,0 +1,34 @@
+const string = "this is a string and this more data"; //
+
+// console.log(string);
+
+// let other = string;
+
+// other = "this is my string now";
+
+// console.log(string);
+// console.log(other);
+
+// console.log(string.length);
+// console.log(string.includes("this  is"));
+// console.log(string.charAt(1));
+// console.log(string.charCodeAt(1));
+// console.log(string.indexOf("is"));
+// console.log(string.lastIndexOf("is"));
+// console.log(string.endsWith(" and more data "));
+// console.log(string.startsWith("this is"));
+// console.log(string.padStart(40));
+// console.log(string.padEnd(35, "*"));
+
+// let trimmed = string.trim();
+// trimmed = string.trimStart();
+// trimmed = string.trimEnd();
+// console.log(trimmed);
+
+// console.log(string.toUpperCase());
+// console.log(string.toLowerCase());
+
+// console.log(string.replace("this", "that"));
+// console.log(string.replaceAll("this", "that"));
+
+// console.log(string.search("this"));
