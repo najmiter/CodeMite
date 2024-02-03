5be610e2ec2fa9dd000d0334d3ee28b260e8239e
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-03 23:39:24 +0500

    Create 022_math.js

---
+++ JavaScript Course/022_math.js
@@ -0,0 +1,15 @@
+const min = Math.min(4, 3, 2, 7, 6, 5);
+const max = Math.max(4, 3, 2, 7, 6, 5);
+const pi = Math.PI;
+const abs = Math.abs(-10);
+const cube = Math.cbrt(8);
+const seel = Math.ceil(12.001);
+const farsh = Math.floor(12.99999999);
+const pow = Math.pow(27, 2);
+const rand = Math.trunc(Math.random() * 1000);
+const round = Math.round(76.5);
+const sroot = Math.sqrt(36);
+const tan = Math.tan(45);
+const farsh2 = Math.trunc(67.99999);
+
+console.log(farsh2);
