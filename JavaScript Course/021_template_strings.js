844a767020638fc458859eabad34db204656bee3
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-03 23:15:19 +0500

    Create 021_template_strings.js

---
+++ JavaScript Course/021_template_strings.js
@@ -0,0 +1,15 @@
+const my_name = "CodeMite";
+const age = -90;
+let string = "My name is " + my_name + ". And my age is " + age;
+
+string = "";
+
+string = `My name is ${my_name.toUpperCase()}. 
+And my age is ${age * -1}.`;
+
+// console.log(string);
+
+// string = `some value
+// new line`;
+
+console.log(string);
