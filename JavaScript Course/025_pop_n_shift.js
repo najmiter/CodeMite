ec873022d9801936653df5fec3967696ec6a543b
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-05 00:12:56 +0500

    Update 025_pop_n_shift.js

--- JavaScript Course/025_pop_n_shift.js
+++ JavaScript Course/025_pop_n_shift.js
@@ -1,8 +1,9 @@
 const array = [1, 2, 3, 4, 5];
 const top = array.pop();
 
-// console.log(top);
+// console.log(top); // 5
+// console.log(array); // [1, 2, 3, 4]
 
 const front = array.shift();
-console.log(front);
-console.log(array);
+console.log(front); // 1
+console.log(array); // // [2, 3, 4]
