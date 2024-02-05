df3cfa7af3783e2c6062f7f6df532d5183934d4d
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-05 22:53:32 +0500

    Create 027_for_of_loops.js

---
+++ JavaScript Course/027_for_of_loops.js
@@ -0,0 +1,14 @@
+const array = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+for (const n of array) {
+    console.log(n);
+}
+
+const string = "CodeMite";
+for (const c of string) {
+    console.log(c);
+}
