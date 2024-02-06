64b85f1d55e71612396e643c292c12bcfdc1b2f1
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-06 22:23:56 +0500

    Create 033_array_slice.js

---
+++ JavaScript Course/033_array_slice.js
@@ -0,0 +1,12 @@
+const a = [1, 2, 3, 4, 5, 6];
+
+let b = a.slice(2, 5);
+// b = a.slice();
+// console.log(a);
+
+// b = a.slice(-4, -2);
+// b = a.slice(4, -2);
+// b = a.slice(3, -2);
+b = a.slice(3);
+
+console.log(b);
