3dbc62f570d7ea5b699c9cc52251254a5f7fe208
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-06 22:43:23 +0500

    Create 034_array_splice.js

---
+++ JavaScript Course/034_array_splice.js
@@ -0,0 +1,15 @@
+// const a = [1, 2, 3, 4, 5, 6];
+
+// let b = a.splice(0, 3);
+
+// b = a.splice(0, 0, 7, 8, 9);
+
+// console.log(a);
+// console.log(b);
+
+const a1 = [1, 2, 3, 4, 5];
+
+const b1 = a1.splice(2, 2, 6, 7, 8, 9);
+
+console.log(a1);
+console.log(b1);
