9376b58df701fc8129065411d9a3c7bcd1a6ba41
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-06 22:57:17 +0500

    Create 035_array_join.js

---
+++ JavaScript Course/035_array_join.js
@@ -0,0 +1,18 @@
+// const stars = ["me", "you", "him", "her"];
+
+// let star_string = "";
+
+// for (const star of stars) {
+//     star_string += star + ", ";
+// }
+
+// console.log(star_string);
+
+// star_string = stars.join(", ");
+
+// console.log(star_string);
+
+const a = [1, 2, 3.5, 4, 5, true, "ak string b hai"];
+
+const joined = a.join(" - ");
+console.log(joined);
