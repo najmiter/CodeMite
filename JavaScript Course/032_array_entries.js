1604243e297e2aebb6b3aa50c169b47b23035092
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-06 22:12:36 +0500

    Create 032_array_entries.js

---
+++ JavaScript Course/032_array_entries.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 3, 4, 5];
+
+for (const [i, n] of array.entries()) {
+    console.log(i, n);
+}
+
+// for (const n of array.values()) {
+//     console.log(n);
+// }
+
+// for (const i of array.keys()) {
+//     console.log(i);
+// }
