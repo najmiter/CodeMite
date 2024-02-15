b828d3c36f15dbf6c294e131041d026c51e72937
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-15 23:15:19 +0500

    Create 038_typeof_instanceof.js

---
+++ JavaScript Course/038_typeof_instanceof.js
@@ -0,0 +1,28 @@
+// const str = true;
+
+// if (typeof str === "string") {
+//     console.log("kch kr lety hn");
+// } else {
+//     console.log(`we don't do that here`);
+// }
+
+// console.log(typeof true);
+
+// console.log(typeof []);
+// const a = [124];
+// // console.log(Array.isArray(a));
+
+// if (Array.isArray(a)) {
+//     /// array k case me
+// }
+
+// if ({} instanceof Array) {
+//     console.log("Array");
+// } else if (new Date() instanceof Date) {
+//     console.log("Date");
+// }
+
+/// ALWAYS RETURNS FALSE (for primitive types)
+// console.log(1 instanceof Number);
+
+console.log(typeof ("12" * 23));
