c83d9caca871d1588f594b2a1b9708e731b62af8
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-24 13:26:42 +0500

    Create 046_array_reverse.js

---
+++ JavaScript Course/046_array_reverse.js
@@ -0,0 +1,12 @@
+const array = [1, 12, -3, 4, 50];
+
+// array.reverse();
+// console.log(array);
+
+// const reversed1 = array.slice().reverse();
+// console.log(array);
+// console.log(reversed1);
+
+const reversed = array.toReversed();
+console.log(array);
+console.log(reversed);
