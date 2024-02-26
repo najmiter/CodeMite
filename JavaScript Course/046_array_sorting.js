a742c70328578fafb3c61800b94afabb8a4b80f8
Najam <85332859+najmiter@users.noreply.github.com>
2024-02-26 13:46:23 +0500

    Update 046_array_sorting.js

--- JavaScript Course/046_array_sorting.js
+++ JavaScript Course/046_array_sorting.js
@@ -1,22 +1,32 @@
-const array = [1, -1, 11, 4, 2, 10];
-// const array2 = ["c", "a", "b"];
+const array = [4, 16, 122, 1, 1, 3];
+// const strings = ["ab", "aa", "ac"];
 
-// console.log(array2);
-// array2.sort();
-// array.sort();
+// console.log(array.sort(compare_numbers));
+// console.log(
+//     array.sort(function (x, y) {
+//         if (x < y) return -1;
+//         return 1;
+//     })
+// );
 
-// const copy = [...array].sort(compare_numbers);
-// const copy1 = array.slice().sort(compare_numbers);
-// array.sort(compare_numbers);
-// console.log(array);
-// console.log(copy);
-// console.log(copy1);
+// console.log(
+//     array.sort((x, y) => {
+//         if (x < y) return -1;
+//         return 1;
+//     })
+// );
 
-const copy2 = array.toSorted(compare_numbers);
-console.log(copy2);
+// console.log(array.sort((x, y) => (x < y ? -1 : 1)));
+
+// console.log(array.sort((x, y) => x - y));
+
+// function compare_numbers(x, y) {
+//     if (x < y) return -1;
+//     else return 1;
+// }
+
+const copy = array.slice().sort((x, y) => x - y);
+const copy1 = [...array].sort((x, y) => x - y);
+const copy2 = array.toSorted((x, y) => x - y);
 console.log(array);
-
-function compare_numbers(x, y) {
-    if (x < y) return -1;
-    else return 1;
-}
+console.log(copy2);
