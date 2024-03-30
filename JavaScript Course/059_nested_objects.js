7aae21dbb2a28a9d712f307dbb6d6b3d15affa07
najmiter <realnajmiter@gmail.com>
2024-03-30 19:24:57 +0500

    nested objects added

---
+++ JavaScript Course/059_nested_objects.js
@@ -0,0 +1,17 @@
+const weather = {
+    current_temp: "19",
+    todays_forcast: {
+        morning: "14",
+        evening: "30",
+        night: "19",
+    },
+};
+
+// weather.todays_forcast.evening = "12423";
+
+console.log(weather.todays_forcast.morning);
+console.log(weather.todays_forcast.evening);
+console.log(weather.todays_forcast.night);
+console.log(weather.todays_forcast);
+
+// console.log(weather.tomorrow.morning); // ERROR!
