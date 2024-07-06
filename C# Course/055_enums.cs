e5566cdf4fca23462549976f9e8f58b3550f1f09
najmiter <realnajmiter@gmail.com>
2024-07-06 19:53:59 +0500

    enums in c#

---
+++ C# Course/055_enums.cs
@@ -0,0 +1,45 @@
+enum Color : byte
+{
+    RED = 10,
+    GREEN = 13,
+    BLUE = 255,
+}
+
+class CodeMite
+{
+    static void Main(string[] _)
+    {
+        // int color = 13;
+        // int red = 10;
+
+        // if (color == red) 
+        // {
+
+        // }
+
+        Color color = Color.RED;
+        // var color2 = Color.BLUE;
+
+        // color = color2;
+
+        // Color.BLUE = Color.GREEN; // NOT ALLOWED
+
+        color = Color.GREEN;
+
+        // if (color == Color.GREEN)
+        // {
+        //     Console.WriteLine($"The color is GREEN");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"The color is {color}");
+        // }
+
+        // Console.WriteLine((int)color);
+        // Console.WriteLine((int)color == 13);
+
+        Console.WriteLine((Color)135);
+        // Console.WriteLine(((Color)10).GetType().Name);
+    }
+
+}
