cda3532f406924c40868448ed8e38370d7a1d372
najmiter <realnajmiter@gmail.com>
2024-07-22 20:30:11 +0500

    instagram video downloader python script

---
+++ miscellaneous/instagram-video-downloaded.py
@@ -0,0 +1,19 @@
+import instaloader
+
+loader = instaloader.Instaloader(    
+    download_pictures=False,
+    download_video_thumbnails=False,
+    download_geotags=False,
+    download_comments=False,
+    save_metadata=False
+)
+
+url = 'https://www.instagram.com/p/C9Sj124xRsw/'
+
+shortcode = url.split("/")[-2]
+
+try:
+    post = instaloader.Post.from_shortcode(loader.context, shortcode)
+    loader.download_post(post, target=shortcode)
+except Exception as e:
+    print(f"An error occurred: {e}")
