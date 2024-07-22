4a036f71465c193b5b3af84c92be84287d31a2db
najmiter <realnajmiter@gmail.com>
2024-07-22 20:42:31 +0500

    instagram video downloader python script

--- miscellaneous/instagram-video-downloaded.py
+++ miscellaneous/instagram-video-downloaded.py
@@ -1,20 +1,23 @@
 import instaloader
 
-loader = instaloader.Instaloader(    
+loader = instaloader.Instaloader(
+    download_comments=False,
+    download_geotags=False,
     download_pictures=False,
     download_video_thumbnails=False,
-    download_geotags=False,
-    download_comments=False,
     save_metadata=False
 )
 
-# replace with any valid post link
+# replace with any valid post link/url
 url = 'https://www.instagram.com/p/C9Sj124xRsw/'
 
-shortcode = url.split("/")[-2]
+shortcode = url.split('/')[-2]
 
 try:
     post = instaloader.Post.from_shortcode(loader.context, shortcode)
+    # target is the folder name where it's going to be saved
+    # you can call it anything you like
     loader.download_post(post, target=shortcode)
+
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f'Something went wrong: {e}')
